2ee9dc80bb5d4b5f010d3ef59e717e1ff50de113
Victoria Oluchi <ikwuegbuoluchiv@gmail.com>
2026-07-22 12:26:02 -0400

    Initial game commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Project-11-0.1.0";
+const contentToCache = [
+    "Build/WebGL_Build.loader.js",
+    "Build/WebGL_Build.framework.js",
+    "Build/WebGL_Build.data",
+    "Build/WebGL_Build.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
